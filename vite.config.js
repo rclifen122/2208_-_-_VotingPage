d2fced808b1aebd87c701f2f678712f40d261c5c
rclifen122 <rclifen@gmail.com>
2025-08-14 15:12:58 +0700

    Update repository name

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Voting_Res/',
+  base: '/2208_-_-_VotingPage/',
   build: {
     outDir: 'dist',
   },
