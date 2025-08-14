c8b1b21c49ccf416eed35a658ae095e805f4456b
rclifen122 <rclifen@gmail.com>
2025-08-14 15:23:20 +0700

    Revert GitHub Pages config for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/2208_-_-_VotingPage/',
+  base: '/',
   build: {
     outDir: 'dist',
   },
